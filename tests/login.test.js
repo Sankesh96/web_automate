8c4b3a546db87f62f1985827a99709b5b275abe0
Sankesh96 <khandaresankesh9995@gmail.com>
2025-02-24 20:03:46 +0530

    login.test.js

--- tests/login.test.js
+++ tests/login.test.js
@@ -6,8 +6,8 @@ async function login(page) {
   const context = await browser.newContext({ ignoreHTTPSErrors: true });
   
   await page.goto('https://scopex.money/Login');
-  await page.fill('[name="username"]', 'email.at.sankesh96@gmail.com');
-  await page.fill('[name="password"]', 'ScopeX$96');
+  await page.fill('[name="username"]', 'test123@gmail.com');
+  await page.fill('[name="password"]', 'Scopex@123');
   await page.click('button:has-text("Log in")');
  // await expect(page).toHaveURL('https://scopex.money/Dashboard');
 }
